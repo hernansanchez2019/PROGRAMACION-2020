3269a879af317fe23825e6b87ab2bd2dfbfe537d
comandos h <hernansanchez_91@hotmail.com.ar>
2020-04-19 22:58:20 -0300

    clases

---
+++ 1 CLASE SABADO/funciones.h
@@ -0,0 +1,19 @@
+void testArray(int parametroArray[],int cantidad);
+
+
+/**
+ *
+ *
+ *
+ * */
+void InicializarArray(int parametroArray[],int cantidad, int valorInicio);
+
+/**
+ *
+ *
+ * */
+void MostarArray(int parametroArray[],int cantidad);
+int SacarLaSuma(int parametroArray[],int cantidad, int valorInicio);
+int SacarCantidadDeNumeros(int parametroArray[],int cantidad, int valorInicio);
+float SacarPromedio(int parametroArray[],int cantidad, int valorInicio);
+int SacarIndiceLibre(int parametroArray[],int cantidad, int valorInicio);
